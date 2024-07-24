4353e855d6d15e7c5f0ba9678b4812a0e8c50cd2
danilo-ck <127243735+danilo-ck@users.noreply.github.com>
2024-07-25 00:24:36 +0200

    Add resizing feature
    A button on the top of the screen is added. It will
    send the user a popup asking for the number of
    squares per side for the new grid. Once entered,
    the existing grid will be removed, and a new grid
    will be generated in the same total space.

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ const container = document.querySelector(".container");
 for(let i = 0; i < 256; i++) {
     const gridElement = document.createElement("div");
     gridElement.classList.add("gridElement");
-    gridElement.textContent = "";
+    gridElement.setAttribute("style", "flex-basis: 6.25%;");
     container.appendChild(gridElement);
 }
 
@@ -13,4 +13,34 @@ gridElements.forEach((gridElement) => {
     gridElement.addEventListener("mouseover", () => {
         gridElement.classList.add("pinted");
     });
+});
+
+const button = document.querySelector(".sizeButton");
+
+button.addEventListener("click", () => {
+    let newSize = prompt("Please enter new size (up to 100):", "16");
+    if(newSize != null && newSize <= 100){
+        let percentaje = 100 / newSize;
+
+        const grid = document.querySelector(".container");
+        while (grid.lastElementChild) {
+            grid.removeChild(grid.lastElementChild);
+        }
+
+        //Creates new divs
+        for(let i = 0; i < (newSize*newSize); i++) {
+            const gridElement = document.createElement("div");
+            gridElement.classList.add("gridElement");
+            gridElement.setAttribute("style", `flex-basis: ${percentaje}%;`);
+            container.appendChild(gridElement);
+        }
+
+        const gridElements = document.querySelectorAll(".gridElement");
+
+        gridElements.forEach((gridElement) => {
+            gridElement.addEventListener("mouseover", () => {
+                gridElement.classList.add("pinted");
+            });
+        });
+    }
 });
