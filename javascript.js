4a47b907aabfb8c0994a8055a86059a70d8b8163
danilo-ck <127243735+danilo-ck@users.noreply.github.com>
2024-07-24 21:26:24 +0200

    Create a 16x16 grid of square divs
    The grid is created using javascript and
    distributed through the page using CSS flexbox
    property.

---
+++ javascript.js
@@ -0,0 +1,8 @@
+const container = document.querySelector(".container");
+
+for(let i = 0; i < 256; i++) {
+    const gridElement = document.createElement("div");
+    gridElement.classList.add("gridElement");
+    gridElement.textContent = "";
+    container.appendChild(gridElement);
+}
